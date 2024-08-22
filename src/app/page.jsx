1bf2af300a846be9d677e45b9461f4c66f856ea3
Muhammad-Haram <haramh643@gmail.com>
2024-08-22 16:55:39 +0500

    Marquee and framer motion

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,14 @@
 import LandingPage from '@/components/LandingPage'
+import Marquee from '@/components/Marquee'
 import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className="w-full h-screen text-white">
+    <div className="w-full min-h-screen text-white bg-zinc-900">
       <Navbar />
       <LandingPage />
+      <Marquee />
     </div>
   )
 }
